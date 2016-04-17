61fa766ae78d17f1f52fb32c6bf8ac1b4012b820
Kazuhiro Furue <k.furue0728@gmail.com>
2016-04-18 01:15:51 +0900

    added using librarytest method to app.

--- app/src/main/java/com/example/furuekazuhiro/libimplementsample/MainActivity.java
+++ app/src/main/java/com/example/furuekazuhiro/libimplementsample/MainActivity.java
@@ -3,11 +3,15 @@ package com.example.furuekazuhiro.libimplementsample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.librarytest.SampleLibrary;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new SampleLibrary().sampleMethod();
     }
 }
