8f21c24886cb335e67daa0c7ce2bf7b8c5335ac2
Kazuhiro Furue <k.furue0728@gmail.com>
2016-04-18 01:13:30 +0900

    added a sample class and method to librarytest.

---
+++ librarytest/src/main/java/com/example/librarytest/SampleLibrary.java
@@ -0,0 +1,13 @@
+package com.example.librarytest;
+
+import android.util.Log;
+
+/**
+ * Created by furuekazuhiro on 2016/04/18.
+ */
+public class SampleLibrary {
+
+    public void sampleMethod() {
+        Log.d("test", "test");
+    }
+}
